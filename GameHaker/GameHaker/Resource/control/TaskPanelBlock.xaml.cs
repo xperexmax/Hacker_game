b300516ee716396566d288dacafbcda0a3861532
brend32 <35214019+brend32@users.noreply.github.com>
2018-10-21 13:29:36 +0300

    ss

--- GameHaker/GameHaker/Resource/control/TaskPanelBlock.xaml.cs
+++ GameHaker/GameHaker/Resource/control/TaskPanelBlock.xaml.cs
@@ -20,9 +20,59 @@ namespace GameHaker
     /// </summary>
     public partial class TaskPanelBlock : UserControl
     {
+
+        #region Ctor   
+
         public TaskPanelBlock()
         {
             InitializeComponent();
+        }
+
+        static TaskPanelBlock()
+        {
+            imageProperty = DependencyProperty.Register("Image", typeof(Brush), typeof(TaskPanelBlock), new FrameworkPropertyMetadata(null, new PropertyChangedCallback(ImagePropertyChange)));
+            notifColorProperty = DependencyProperty.Register("NotifColor", typeof(Color), typeof(TaskPanelBlock), new FrameworkPropertyMetadata(Colors.White, new PropertyChangedCallback(NotifColorPropertyChange)));
+            
+        }
+
+        #endregion
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty imageProperty;
+        public static readonly DependencyProperty notifColorProperty;
+
+        #endregion
+
+
+        #region Propetries
+
+        public Brush Image
+        {
+            get { return (Brush)GetValue(imageProperty); }
+            set { SetValue(imageProperty, value); }
+        }
+
+        public Color NotifColor
+        {
+            get { return (Color)GetValue(notifColorProperty); }
+            set { SetValue(notifColorProperty, value); }
+        }
+
+        #endregion
+
+        private static void NotifColorPropertyChange(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TaskPanelBlock c = obj as TaskPanelBlock;
+            Color v = (Color)e.NewValue;
+            c.notif.Fill = new SolidColorBrush(v);
+        }
+
+        private static void ImagePropertyChange(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TaskPanelBlock c = obj as TaskPanelBlock;
+            Brush v = (Brush)e.NewValue;
+            c.image.Fill = v;
         }
     }
 }
